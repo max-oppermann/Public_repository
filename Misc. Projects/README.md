20dc5969478b6864c1d2d098ba8391609283d350
Max Oppermann <oppermann-max@gmx.de>
2024-11-08 13:24:12 +0100

    added monty_hall.py

--- Misc. Projects/README.md
+++ Misc. Projects/README.md
@@ -1,5 +1,8 @@
 Programs of varying length and complexity without overarching theme.
 
 #### `matching_cards.py`
-Simulating the matching cards problem: Shuffle a deck of $N$ cards, labeled 1 through $N$. What is the probability that at least one card in position $i$ has $i$ written on it (turning over the seventh card and it has a 7 on it, e. g., but also drawing 1, 2, 3, 4, ...)?  
-Solving this analytically gives $1-\frac1e\approx0.63$, which is in fact what the program converges to for simulating the problem often enough. "Often enough" turns out to be at least 10,000 times. I also plot the convergence behavior from 10 to 10,000 replications.
+Simulates the matching cards problem: Shuffle a deck of $N$ cards, labeled 1 through $N$. What is the probability that at least one card in position $i$ has $i$ written on it (turning over the seventh card and it has a 7 on it, e. g., but also drawing 1, 2, 3, 4, ...)?  
+Solving this analytically gives $1-\frac1e\approx0.63$, which is in fact what the program converges to for simulating the problem often enough. "Often enough" turns out to be at least 10,000 times. I also plot the convergence behavior from 10 to 10,000 replications.  
+
+#### `monty_hall.py`  
+Simulates the classic Monty Hall problem, allowing you to play multiple rounds. The car is randomly placed behind one of three doors, you are asked to choose one. After your choice, Monty reveals a non-car door, and you are given the option to switch. The program tracks your wins and losses, and also compares your results with theoretical probabilities of winning by always staying or always switching (simulates 1000 rounds of always staying/switching). The simulation continues until you decide to stop.
---
+++ Misc. Projects/monty_hall.py
@@ -0,0 +1,108 @@
+"""
+Let's you play through the Monty Hall problem with three doors as often as you like.
+Car is behind a uniformly random door. Monty deterministically chooses the last non-car
+door if the player chose a non-car door and chooses a door uniformly at random if player 
+chose the car door initially.
+Demonstrates to those without faith in conditional probability that switching > staying.
+
+Original R-code from:
+Blitzstein; Hwang "Introduction to probability." 2019. p. 82–83.
+Their code only lets you play one round and does not give you your win/loss ratio,
+but the code for getting the theoretical proportions (in my main() function) is theirs
+almost verbatim.
+"""
+
+import numpy as np
+
+DOORS = [1, 2, 3]
+
+
+def main():
+    """
+    Starts the Monty Hall game loop, allowing the player to play multiple rounds.
+
+    The game continues until the player decides to stop, tallies wins and rounds
+    and shows the proportion of wins.
+    Shows what would happen if you played 1000 rounds always/never switching.
+    """
+    num_wins = 0
+    num_rounds = 0  # this is at least 1 since you're forced to play a round
+
+    # counting wins and rounds; everything else catches errors
+    while True:
+        num_wins += monty_hall()
+        num_rounds += 1
+        while True:
+            player_status = input("Keep playing (y/n)? ").strip().lower()
+            if player_status[0] in ['y', 'n']:  # first letter
+                break
+            print("Illegitimate input\n")
+        if player_status.startswith("n"):
+            break
+
+    # get theoretical proportions:
+    # A vector 1000 long of numbers 1, 2, or 3.
+    # W.l.o.g. we can assume player chooses 1; then staying only wins if we get a 1
+    # and switching only loses when we get a 1.
+    door_vector = np.random.choice(DOORS, 1000, replace=True)
+    proportion_stay = np.sum(door_vector == 1) / 1000
+    proportion_switch = 1 - proportion_stay
+
+    # printing results
+    print(f"Your win/loss ration: {num_wins/num_rounds:.3f}")
+    print(f"Ratio if you always stay: {proportion_stay:.3f}")
+    print(f"Ratio if you always switch: {proportion_switch:.3f}")
+
+
+def monty_hall():
+    """
+    Let's You play exactly one round of Monty Hall with 3 doors.
+
+    Picks a door of 1, 2, 3 at random, then lets player choose a door, catching input errors.
+    Then chooses monty's door from the remaining doors (which may be only one).
+    Then asks player whether they want to switch, again catching input errors.
+    Returns integer 1 or 0 for win and loss respectively.
+    """
+
+    # pick where the car is
+    car_door = np.random.choice(DOORS)
+
+    # get player's door
+    print("Monty Hall says, 'Pick a door!'")
+    while True:
+        try:
+            chosen_door = int(input("Choose a door (1, 2, or 3): "))
+            if chosen_door in DOORS:
+                break
+            print("Input must be 1, 2, or 3.")
+        except ValueError:
+            print("Invalid input. Please enter an integer (1, 2, or 3).")
+
+    # get Monty's door (not player's door or the car door)
+    if chosen_door == car_door:
+        monty_door = np.random.choice(list(set(DOORS) - {chosen_door}))
+    else:
+        monty_door = list(set(DOORS) - {chosen_door, car_door})[0]
+    print(f"Monty opens door {monty_door}!")
+
+    # ask for switch
+    while True:
+        reply = input("Would you like to switch (y/n)? ").strip().lower()
+        if reply[0] in ['y', 'n']:  # first letter
+            break
+        print("Illegitimate input")
+
+    # if player chooses to switch, select other door
+    if reply.startswith("y"):
+        chosen_door = list(set(DOORS) - {chosen_door, monty_door})[0]
+
+    # result of the game
+    if chosen_door == car_door:
+        print("You won!")
+        return 1
+    print("You lost!")
+    return 0
+
+
+if __name__ == "__main__":
+    main()
